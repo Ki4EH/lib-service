f1121d18dbb783d6ef6258a48e23c868dd941fb3
livmas <seiranyan.sg@gmail.com>
2023-04-03 11:08:28 +0300

    Search system update
    Created:
    /search request now handle parameter author;
    Scanning book genres now provided by func scanGenres;
    Func searchByAuthor let you search by author;
    Func search now divided into 4 main cases:
    -title and author are provided,
    -T is provided, A isn't,
    -A is provided, T isn't,
    -Nothing provided (return nil);
    
    Fixed:
    Error handlers are added in some places;
    Struct Book is moved to book.go;
    Every get request now let you get author name;

--- microservises/catalog/catalog.go
+++ microservises/catalog/catalog.go
@@ -4,17 +4,9 @@ import (
 	"database/sql"
 	"encoding/json"
 	"net/http"
+	"reflect"
 	"strconv"
 )
-
-type Book struct {
-	ID     int      `json:"id"`
-	Author string   `json:"authors"`
-	Title  string   `json:"title"`
-	ISBN   string   `json:"isbn"`
-	Count  int      `json:"count"`
-	Genres []string `json:"genres"`
-}
 
 // RunCatalogHandler Описывает обработку http запросов
 func RunCatalogHandler(db *sql.DB) {
@@ -29,44 +21,48 @@ func RunCatalogHandler(db *sql.DB) {
 		if err1 != nil {
 			panic(err1)
 		}
-		writer.Write(res)
+		_, err2 := writer.Write(res)
+		if err2 != nil {
+			return
+		}
 	})
 
 	http.HandleFunc("/search", func(writer http.ResponseWriter, request *http.Request) {
-		name := request.URL.Query().Get("name")
-		books := search(db, name)
+		name := request.URL.Query().Get("title")
+		author := request.URL.Query().Get("author")
+		books := search(db, name, author)
 
 		js, err := json.Marshal(books)
 		if err != nil {
 			panic(err)
 		}
-		writer.Write(js)
+		_, err1 := writer.Write(js)
+		if err1 != nil {
+			return
+		}
 	})
+	//http.HandleFunc("/book", func(writer http.ResponseWriter, request *http.Request) {
+	//	if request.Method == "POST" {
+	//	}
+	//})
 }
 
 func getBookByID(db *sql.DB, id int) Book {
 	// Получение от бд основной инфы про книгу, заносится в переменную book
 	rows := db.QueryRow("select * from books where id = $1", id)
 	book := Book{}
-	err := rows.Scan(&book.ID, &book.Title, &book.Count)
+	var nullAuthor sql.NullString
+	err := rows.Scan(&book.ID, &book.Title, &book.Count, &nullAuthor)
+	if reflect.TypeOf(nullAuthor) == nil {
+		book.Author = ""
+	} else {
+		book.Author = nullAuthor.String
+	}
+
 	if err != nil {
 		panic(err)
 	}
-
-	// Получения жанров книги
-	genres, err1 := db.Query("SELECT name FROM genre_book JOIN genres g ON g.id = genre_book.genre_id WHERE book_id = $1", id)
-	if err1 != nil {
-		panic(err1)
-	}
-
-	// Занесение жанров в переменную book
-	var gens []string
-	for genres.Next() {
-		var g string
-		genres.Scan(&g)
-		gens = append(gens, g)
-	}
-	book.Genres = gens
+	scanGenres(db, &book)
 
 	return book
 }
@@ -74,17 +70,33 @@ func getBookByID(db *sql.DB, id int) Book {
 // Через эту функцию будет осуществляться основной поиск в бд.
 // Внутри нее будут вызываться остальные функции, связанные с поиском,
 // со временем ее функционал будет наращиваться.
-func search(db *sql.DB, title string) []Book {
+func search(db *sql.DB, title string, author string) []Book {
 	var result []Book
 
-	// Трансляция всех элементов из резяльтата поиска по названию в массив result
-	for _, elem := range searchByTitle(db, title) {
-		result = append(result, elem)
+	if title != "" {
+		if author == "" {
+			// Трансляция всех элементов из резяльтата поиска по названию в массив result
+			result = searchByTitle(db, title)
+		}
+		//else {
+		//	result = searchByTnA(db, title, author)
+		//}
+	} else {
+		if author != "" {
+			result = searchByAuthor(db, author)
+		} else {
+			result = nil
+		}
 	}
 
-	// Здесь буут применяться другие функции поиска,
+	// Здесь будут применяться другие функции поиска,
 	// которые будут вносить изменения в массив result.
 	// Возможно эту систему поиска потом поменяем
+
+	for i := range result {
+		b := &result[i]
+		scanGenres(db, b)
+	}
 
 	return result
 }
@@ -92,18 +104,57 @@ func search(db *sql.DB, title string) []Book {
 // Поиск по названию :\
 func searchByTitle(db *sql.DB, title string) []Book {
 	var books []Book
-	rows, err := db.Query("SELECT * FROM books WHERE title = $1", title)
+	rows, err := db.Query("SELECT books.id, title, count, name FROM books JOIN authors a ON a.id = books.author_id WHERE title = $1", title)
 	if err != nil {
 		panic(err)
 	}
 
 	for rows.Next() {
 		var book Book
-		rows.Scan(&book.ID, &book.Title, &book.Count)
+		err := rows.Scan(&book.ID, &book.Title, &book.Count, &book.Author)
+		if err != nil {
+			panic(err)
+		}
 		books = append(books, book)
 	}
-
 	return books
+}
+
+func searchByAuthor(db *sql.DB, author string) []Book {
+	var books []Book
+	rows, err := db.Query("SELECT books.id, title, count, name FROM books JOIN authors a ON books.author_id = a.id WHERE a.name = $1", author)
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		var book Book
+		err = rows.Scan(&book.ID, &book.Title, &book.Count, &book.Author)
+		if err != nil {
+			panic(err)
+		}
+		books = append(books, book)
+	}
+	return books
+}
+
+func scanGenres(db *sql.DB, book *Book) {
+	// Получения жанров книги
+	genres, err1 := db.Query("SELECT name FROM genre_book JOIN genres g ON g.id = genre_book.genre_id WHERE book_id = $1", book.ID)
+	if err1 != nil {
+		panic(err1)
+	}
+	// Занесение жанров в переменную book
+	var gens []string
+	for genres.Next() {
+		var g string
+		err := genres.Scan(&g)
+		if err != nil {
+			panic(err)
+		}
+		gens = append(gens, g)
+	}
+	book.Genres = gens
 }
 
 //
