c4efe58e6686d0cf1c8ce78c76357d9c93cbcff6
YoungPentagonHacker <84976745+YoungPentagonHacker@users.noreply.github.com>
2023-05-21 23:26:34 +0300

    Update main.go

--- cmd/catalog/main.go
+++ cmd/catalog/main.go
@@ -10,11 +10,11 @@ import (
 
 func main() {
 	// Retrieve secrets from environment variables
-	host := "95.140.159.168"
-	port := "5433"
-	user := "go_project"
-	password := "rIo3Fc"
-	dbname := "lib-service-test"
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
 
 	// Construct the connection string
 	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
